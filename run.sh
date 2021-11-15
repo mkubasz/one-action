412f4628734aeac0cfa1bef9caf22c6df002711c
Mateusz <mkubasz@gmail.com>
2021-11-15 21:24:19 +0100

    add script

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 echo "Test"
+for v in `env`; do
+  PREFIX=$(echo $v| cut -d '_' -f1)
+  if [[ $PREFIX == 'GITHUB' ]] || [[ $PREFIX == 'INPUT' ]]; then
+    echo "$v";
+  fi
+done
+
+echo "Event"
+
+jq $INPUT_MY_SELECTOR $GITHUB_EVENT_PATH
